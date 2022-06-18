07d9bc3568e133dd6cbe95b70b2bc6d1cfc7adb4
buse tuba kocabıyık <btubakocabiyik@gmail.com>
2022-06-19 00:20:11 +0300

    Add Test Project

---
+++ Test/Program.cs
@@ -0,0 +1,18 @@
+﻿using LogLib;
+using DocumentManagementLib;
+
+LoggerFactory Factory = new LoggerFactory("config.json", Format.JSON);
+Logger logger = Factory.CreateLogger();
+
+Document document = new Document();
+User user = new User("inc", Department.IT,"Susan", logger);
+
+user.LoadDocument("YENİ");
+user.ChangeDocumentName(document,"Yeni1");
+user.ChangeDocumentName(document, "yeni2");
+user.UndoDocumentName(document);
+Console.WriteLine(document.Name);
+
+Report report = Report.Instance;
+
+Console.WriteLine();
